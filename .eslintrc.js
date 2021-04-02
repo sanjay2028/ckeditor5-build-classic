0855b0605a7517f33af1131666261d438a079407
Sanjay Kumar <sanjay.kumar@enabledata.com>
2021-04-02 17:06:18 +0530

    addition

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,8 @@
 'use strict';
 
 module.exports = {
-	extends: 'ckeditor5'
+	extends: 'ckeditor5',
+	rules: {
+		"linebreak-style": ["error", "unix"]
+	}
 };
--- src/ckeditor.js
+++ src/ckeditor.js
@@ -29,6 +29,8 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import PageBreak from '@ckeditor/ckeditor5-page-break/src';
+import MathType from '@wiris/mathtype-ckeditor5/src/plugin';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -56,7 +58,9 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+	PageBreak,
+	MathType
 ];
 
 // Editor configuration.
